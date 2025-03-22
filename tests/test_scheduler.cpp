3df8b704c4792be60c0eb4d06429f568b7a9f399
lsh <1571374817@qq.com>
2025-03-22 15:04:45 +0800

    增加scheduler

---
+++ tests/test_scheduler.cpp
@@ -0,0 +1,23 @@
+#include "log.h"
+#include "scheduler.h"
+
+std::shared_ptr<lsh::Logger> g_logger = LSH_LOG_ROOT;
+
+void test_fiber() {
+    static int count = 5;
+    LSH_LOG_INFO(g_logger) << "test in fiber count=" << count;
+
+    sleep(1);
+    if (--count >= 0) {
+        lsh::Scheduler::GetThis()->schedule(&test_fiber, lsh::GetThreadId());
+    }
+}
+
+int main(int argc, char **argv) {
+    lsh::Scheduler sc(3, false, "test");
+    sc.start();
+    sc.schedule(&test_fiber);
+    sc.stop();
+    LSH_LOG_INFO(g_logger) << "over";
+    return 0;
+}
